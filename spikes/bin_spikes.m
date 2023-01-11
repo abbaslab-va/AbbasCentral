be4c76fcac89ce0c9507f2709477ef8f816d0a8f
Lowell Bartlett <bartlett.lowell@gmail.com>
2023-01-11 15:44:59 -0800

    Modified the bin_spikes function to accept a bin width parameter instead of a sampling frequency for output

--- spikes/bin_spikes.m
+++ spikes/bin_spikes.m
@@ -7,20 +7,21 @@ they wish to bin.
 INPUT: 
     spikeTimes - 1xS vector where S is the number of spikes in the input
     edges - 1x2 vector [start_time, end_time], where each time is a
-    reference time in seconds from member edgeRef
+    reference time in seconds from variable edgeRef
     edgeRef - a time-point in the original sampling frequency that you wish
     to align spikes in spikeTimes to
     res - original sampling resolution in hertz
-    freq - desired output frequency in hertz
+    binWidth
 
 OUTPUT:
     binnedSpikes - 1xT vector where T = number of bins (time x frequency)
     Each value will be a one (spike) or a zero
 %}
 
-function binnedSpikes = bin_spikes(spikeTimes, edges, edgeRef, res, freq)
+function binnedSpikes = bin_spikes(spikeTimes, edges, edgeRef, res, binWidth)
     edgeStart = edgeRef + edges(1)*res;
     edgeEnd = edgeRef + edges(2)*res;
-    binEdges = edgeStart:1*res/freq:edgeEnd;  % bin steps are bin frequency
+    stepSize = floor(res/1000*binWidth);
+    binEdges = edgeStart:stepSize:edgeEnd;  % bin steps are bin frequency
     binnedSpikes = histcounts(spikeTimes, 'BinEdges', binEdges);
 end
