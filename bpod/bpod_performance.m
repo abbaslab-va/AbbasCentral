7ac728be25f5c73e6399aa5ccced72c24fc535c4
Lowell Bartlett <bartlett.lowell@gmail.com>
2023-01-13 11:12:00 -0800

    Publish a function that calculates performance for a desired outcome of all trial types over a Bpod session

---
+++ bpod/bpod_performance.m
@@ -0,0 +1,38 @@
+%{
+Lowell Bartlett - 12/09/2022 - Abbas Lab
+This function returns a vector by trial type of the number of completed
+trials of each trial type, as well as the number correctly completed for
+the trial type of interest.
+
+INPUT: 
+    bpodSession - a Bpod behavior session file
+    correctOutcome - optionally include an integer value in your function call to
+    calculate performance outcomes other than 1.
+OUTPUT:
+    numTT - 1xT vector where T is the number of trial types. Stores # of each
+    trial type completed in bpodSession
+    numCorrect - 1xT vector where T is the number of trial types. Stores #
+    of trial types with the outcome specified by varargin. If no argument
+    is given, the default outcome returned by numCorrect is for 1.
+%}
+
+function [numTT, numCorrect] = bpod_performance(bpodSession, correctOutcome)
+    if ~exist('correctOutcome', 'var')
+        correctOutcome = 1;
+    end
+    %Must have the SessionPerformance variable saved to the Bpod structure to use this function
+    perfException = MException('MATLAB:missingVariable', 'Error: No SessionPerformance variable found in bpodSession');
+    %User can enter an integer value as the second argument to change the default outcome evaluation
+    nTrials = bpodSession.nTrials;
+    trialTypes = bpodSession.TrialTypes;
+    nTT = max(trialTypes);
+    if ~isfield(bpodSession, 'SessionPerformance')
+        throw(perfException)
+    end
+    correctChoice = numel(find(bpodSession.SessionPerformance == correctOutcome));
+    for tt = 1:nTT
+        ttInd = find(bpodSession.TrialTypes == tt);
+        numTT(tt) = numel(ttInd);
+        numCorrect(tt) = numel(find(bpodSession.SessionPerformance(ttInd) == correctOutcome));
+    end
+end
