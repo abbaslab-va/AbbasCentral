c7a0e4d781c2c0e13c7ebad899a757348b56d416
Lowell Bartlett <bartlett.lowell@gmail.com>
2023-01-19 14:56:53 -0800

    Update monosynpatic correlation function

--- class_development/NeurDat.m
+++ class_development/NeurDat.m
@@ -19,9 +19,10 @@ classdef NeurDat < BehDat
             window = window*baud;
             spikeSubset = cellfun(@(x) x(x >= window(1) & x < window(2)), obj.spikes, 'uni', 0);
             numNeurons = numel(obj.spikes);
-            for n = 1:numNeurons
-                h = figure;
-                rasterplot(spikeSubset{n}, )
+%             for n = 1:numNeurons
+%                 h = figure;
+%                 rasterplot(spikeSubset{n}, )
+%             end
         end
 
         function binnedSpikes = bin_spikes(obj, binEdges, binSize)
@@ -34,10 +35,9 @@ classdef NeurDat < BehDat
             end
         end
 
-        function stepwiseAll = corn_mono(obj)
+        function stepwiseAll = find_mono(obj)
             binSize = 1;        %ms
             corrWidth = 50;     %ms
-            wideVals = corrWidth - 10;
             numSeconds = floor(obj.frames/obj.baud);
             numNeurons = numel(obj.spikes);
             binnedSpikes = obj.bin_spikes([0 numSeconds], binSize);
@@ -45,20 +45,29 @@ classdef NeurDat < BehDat
             for ref = 1:numNeurons
                 target = ref+1;
                 while target <= numNeurons
-                    fullCorr = xcorr(binnedSpikes(ref, :), binnedSpikes(target, :), )
-                    stepwiseCorr = zeros(numSeconds, corrWidth*2 + 1);
-                    for sec = 1:numSeconds
-                        window = (1:1000/binSize) + (sec-1)*1000/binSize;
-                        stepwiseCorr(sec, :) = round(xcorr(binnedSpikes(ref, window), ...
-                            binnedSpikes(target, window), corrWidth));
+                    fullCorr = xcorr(binnedSpikes(ref, :), binnedSpikes(target, :), corrWidth);
+                    wideCorr = [fullCorr(1:corrWidth-10), fullCorr(end-corrWidth+1:end)];
+                    wideMean = mean(wideCorr);
+                    wideSTD = std(wideCorr);
+                    if any(fullCorr(corrWidth-2:corrWidth) > wideMean + 3*wideSTD) && ...
+                            sum(fullCorr) > 1000 && ...
+                            wideMean > 3* wideSTD && ...
+                            ~any(find(fullCorr == max(fullCorr)) == corrWidth + 1)
+                        stepwiseCorr = zeros(numSeconds, corrWidth*2 + 1);
+                        for sec = 1:numSeconds
+                            window = (1:1000/binSize) + (sec-1)*1000/binSize;
+                            stepwiseCorr(sec, :) = round(xcorr(binnedSpikes(ref, window), ...
+                                binnedSpikes(target, window), corrWidth));
+                        end
+                        stepwiseAll{ref, target} = stepwiseCorr;
                     end
-                    stepwiseAll{ref, target} = stepwiseCorr;
                     target = target+1;
                 end
             end
+            find_mono(stepwiseAll)
         end
         
-        function corn_inhib
+        function find_inhib
         end
     end
 end
