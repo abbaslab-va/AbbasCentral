a61149956b1047444b2485d75fc1dca5fbe53940
Randall Olson <rolsonjames@gmail.com>
2023-03-15 14:39:41 -0700

    add condition if events are less than three to skip session and fill with nans

--- @BehDat/ppc.m
+++ @BehDat/ppc.m
@@ -1,6 +1,6 @@
 function [ppc, spikePhase] = ppc(obj, event, varargin)
 
-
+disp(obj.info.path)
 % INPUT:
 %     event - a string of a state named in the config file (required)
 %     name-value pairs:
@@ -46,6 +46,7 @@ eventTimes = obj.find_event(a.event, 'trialType', a.trialType, 'outcome', a.outc
 a.edges = (a.edges * baud) + eventTimes';
 edgeCells = num2cell(a.edges, 2);
 
+
 % navigate to subject folder and load LFP
 [parentDir, sub] = fileparts(obj.info.path);
 NS6 = openNSx(fullfile(parentDir, sub, strcat(sub, '.ns6')));
@@ -55,41 +56,49 @@ clear NS6
 numChan = size(lfp, 1);
 phase = cell(1, numChan);
 
-% calculate phase
-for c = 1:numChan
-    chanPhase= cellfun(@(x) angle(hilbert(bandpass(lfp(c, x(1):x(2)-1), a.freqLimits,sf))), edgeCells, 'uni', 0);
-    chanPhase = cat(3, chanPhase{:});
-    phase{c} = num2cell(squeeze(chanPhase),1);
-end
-
-
-% find spike times arpund event and zero to start of event
-for n = 1:length(obj.spikes)
-    spikes= cellfun(@(x) obj.spikes(n).times(find(obj.spikes(n).times>x(1) & obj.spikes(n).times<x(2))), edgeCells,'uni',0');
-    spikesZeroed{n}=cellfun(@(x,y) x-y(1),spikes,edgeCells,'uni',0)';
-end
-
-
-% index spike times to LFP
-for c=1:numChan
-    for n=1:length(obj.spikes)
-        spikePhaseTemp=cellfun(@(x,y) x(y), phase{c},spikesZeroed{n},'uni' ,0) ;
-        spikePhaseTemp=cat(1,spikePhaseTemp{:});
-        if isempty(spikePhaseTemp)
-            spikePhase{c,n}=NaN;
-        else
-            spikePhase{c,n}= spikePhaseTemp;
+%check is num event is less than three, if true populate with nans 
+numSpikes=length(obj.spikes);
+if numel(edgeCells)<3
+   ppc=cell(numChan,numSpikes);
+   spikePhase=cell(numChan,numSpikes);
+   [ppc{:}]=deal(NaN);
+   [spikePhase{:}]=deal(NaN);
+else 
+    % calculate phase
+    for c = 1:numChan
+        chanPhase= cellfun(@(x) angle(hilbert(bandpass(lfp(c, x(1):x(2)-1), a.freqLimits,sf))), edgeCells, 'uni', 0);
+        chanPhase = cat(3, chanPhase{:});
+        phase{c} = num2cell(squeeze(chanPhase),1);
+    end
+    
+    
+    % find spike times arpund event and zero to start of event
+    for n = 1:length(obj.spikes)
+        spikes= cellfun(@(x) obj.spikes(n).times(find(obj.spikes(n).times>x(1) & obj.spikes(n).times<x(2))), edgeCells,'uni',0');
+        spikesZeroed{n}=cellfun(@(x,y) x-y(1),spikes,edgeCells,'uni',0)';
+    end
+    
+    
+    % index spike times to LFP
+    for c=1:numChan
+        for n=1:length(obj.spikes)
+            spikePhaseTemp=cellfun(@(x,y) x(y), phase{c},spikesZeroed{n},'uni' ,0) ;
+            spikePhaseTemp=cat(1,spikePhaseTemp{:});
+            if isempty(spikePhaseTemp)
+                spikePhase{c,n}=NaN;
+            else
+                spikePhase{c,n}= spikePhaseTemp;
+            end 
         end 
     end 
+    
+    
+    % find significant ppc 
+    sigPhase= cellfun(@(x) circ_rtest(x),spikePhase);
+    
+    % make nonsignificant ppc NaN
+    ppc=cellfun(@(x) mean(nonzeros(triu(cos(x.'-x),1))),spikePhase);
+    
+    ppc(sigPhase>0.05)=NaN;
+
 end 
-
-
-% find significant ppc 
-sigPhase= cellfun(@(x) circ_rtest(x),spikePhase);
-
-% make nonsignificant ppc NaN
-ppc=cellfun(@(x) mean(nonzeros(triu(cos(x.'-x),1))),spikePhase);
-
-ppc(sigPhase>0.05)=NaN;
-
-
