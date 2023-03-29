1f5d5b820b93f4fe880bb8fae96c17ee378367de
Lowell Bartlett <bartlett.lowell@gmail.com>
2023-03-29 10:23:42 -0700

    Fix trialize_rotation to output matrix
    You can now use that function to output a matrix for a heatmap. It still needs to be modified to accept trialType and outcome parameters

--- @BehDat/trialize_rotation.m
+++ @BehDat/trialize_rotation.m
@@ -14,7 +14,7 @@ function rotVec = trialize_rotation(obj, stateName, varargin)
 %     'trialType' - a trial type found in config.ini
 %     'eos' - a boolean that if true, aligns to the end of a state rather than the start
 
-defaultEdges = [-2 2];          % seconds
+defaultEdges = [0 1];          % seconds
 defaultOffset = 0;              % offset from event in seconds
 defaultOutcome = [];            % all outcomes
 defaultTrialType = [];          % all TrialTypes
@@ -32,8 +32,8 @@ addParameter(p, 'eos', defaultEOS, @islogical);
 parse(p, stateName, varargin{:});
 
 a = p.Results;
-event = a.stateName;
-edges = a.edges;
+stateName = a.stateName;
+edges = floor(a.edges * obj.info.baud);
 trialType = a.trialType;
 outcome = a.outcome;
 offset = a.offset;
@@ -42,6 +42,31 @@ alignToEnd = a.eos;
 bodyAngles = get_body_angle(obj.coordinates);
 [firstFrame, firstTrial] = find_first_frame(obj.bpod);
 framesByTrial = align_frames_to_trials(obj.bpod, size(obj.coordinates, 1), firstTrial);
+framesBack = edges(1);
+framesForward = edges(2);
 
-% if alignToEnd
-%     frameIdx = extract_field_frames_eof()
+if alignToEnd || edges(1) < 0
+    frameIdx = extract_field_frames_eof(obj.bpod, framesByTrial, framesBack, stateName);
+else
+    frameIdx = extract_field_frames(obj.bpod, framesByTrial, stateName);
+end
+
+% This next block of code is written to specifically interface with video
+% files recorded during NMTP_Outer_Training2 behavior. It will align the
+% video to the behavior in the event that recording started before the bpod
+% behavior was initialized.
+firstDelay = obj.bpod.RawEvents.Trial{1, 1}.States.WaitForChoicePoke(1, 1);
+firstDelayFrames = floor(firstDelay * 30);
+cameraOffset = 0;
+%Align videos that were started before the behavior session
+if firstTrial == 1 && firstFrame < .1
+    framesEarly = delay_from_video(vid);
+    cameraOffset = framesEarly - firstDelayFrames;
+end
+
+frameIdx = frameIdx + floor(offset*obj.info.baud) + cameraOffset;
+frameIdx(frameIdx >= size(obj.coordinates, 1) - framesForward) = [];
+frameIdx = num2cell(frameIdx);
+rotVec = cellfun(@(x) bodyAngles(x:x+framesForward), frameIdx, 'uni', 0);
+rotVec = cellfun(@(x) x - x(1), rotVec, 'uni', 0);
+rotVec = cat(2, rotVec{:})';
--- BehDatHelper/get_body_angle.m
+++ BehDatHelper/get_body_angle.m
@@ -1,15 +1,18 @@
 function bodyAngles = get_body_angle(coordMat)
 
-% earLine = obj.coordinates(:, [5 6]) - obj.coordinates(:, [3 4]);
-% [thetaEars, rho] = cart2pol(earLine(:, 1), earLine(:, 2));
-% angleEars = unwrap(thetaEars);
-% 
-% centerLine = obj.coordinates(:, [7 8]) - obj.coordinates(:, [1 2]);
-% [thetaCenter, rho] = cart2pol(centerLine(:, 1), centerLine(:, 2));
-% angleCenter = unwrap(thetaCenter);
-
+earLine = coordMat(:, [5 6]) - coordMat(:, [3 4]);
+thetaEars = cart2pol(earLine(:, 1), earLine(:, 2));
+centerLine = coordMat(:, [7 8]) - coordMat(:, [1 2]);
+thetaCenter = cart2pol(centerLine(:, 1), centerLine(:, 2));
+angleCenter = unwrap(thetaCenter);
+headCrossAngle = thetaCenter - thetaEars;
+withinTolerance = headCrossAngle < -1.5*pi + .2 & headCrossAngle > -1.5*pi - .2;
+headCrossAngle(withinTolerance) = headCrossAngle(withinTolerance) + 2*pi;
+rightAngle = 0.5*pi;
+useCenter = headCrossAngle < rightAngle + .2 & headCrossAngle > rightAngle - 2;
 % mid back compared to skull seems to work best for now
 bodyLine = coordMat(:, [11 12]) - coordMat(:, [7 8]);
-[thetaBody, rho] = cart2pol(bodyLine(:, 1), bodyLine(:, 2));
+thetaBody = cart2pol(bodyLine(:, 1), bodyLine(:, 2));
+thetaBody(useCenter) = thetaCenter(useCenter);
 bodyAngles = unwrap(thetaBody);
-
+% bodyAngles(useCenter) = angleCenter(useCenter);
