b57d043585c190609c66bc512907c9be64f9a28d
Lowell Bartlett <bartlett.lowell@gmail.com>
2023-05-09 14:25:42 -0700

    Add conditional excludeEventsByState flag, remove sampling rate correction for now

--- @BehDat/find_bpod_event.m
+++ @BehDat/find_bpod_event.m
@@ -12,6 +12,7 @@ function timestamps = find_bpod_event(obj, event, varargin)
 p = parse_BehDat('event', 'offset', 'outcome', 'trialType', 'trials');
 % addParameter(p, 'priorToState', [], @ischar);
 addParameter(p, 'priorToEvent', [], @ischar);
+addParameter(p, 'excludeEventsByState', [], @ischar);
 addParameter(p,'trialized', false, @islogical);
 
 parse(p, event, varargin{:});
@@ -24,6 +25,7 @@ trials = a.trials;
 priorToEvent = a.priorToEvent;
 trialized = a.trialized;
 rawEvents = obj.bpod.RawEvents.Trial;
+excludeEventsByState = a.excludeEventsByState;
 
 % Find trial start times in acquisition system timestamps
 trialStartTimes = obj.find_event('Trial Start');
@@ -101,13 +103,24 @@ eventTimes2Check = eventTimes(goodTrials);
 bpodEventTimes = cellfun(@(x) [x{:}], eventTimes2Check, 'uni', 0);
 
 
-% Need to complete this section
-if ~isempty(priorToEvent)
-    priorToEventTimes = cellfun(@(x) x.Events.(priorToEvent)(1, 1), rawEvents2Check, 'uni', 0);
-    priorToDiff = cellfun(@(x, y) x - y, priorToEventTimes, bpodEventTimes, 'uni', 0);
-    goodTimes = cellfun(@(x) find(x == min(x(x >= 0))), priorToDiff, 'uni', 0);
-    poop = cellfun(@(x, y) x(y), bpodEventTimes, goodTimes);
+% % Need to complete this section
+% if ~isempty(priorToEvent)
+%     priorToEventTimes = cellfun(@(x) x.Events.(priorToEvent)(1, 1), rawEvents2Check, 'uni', 0);
+%     priorToDiff = cellfun(@(x, y) x - y, priorToEventTimes, bpodEventTimes, 'uni', 0);
+%     goodTimes = cellfun(@(x) find(x == min(x(x >= 0))), priorToDiff, 'uni', 0);
+%     poop = cellfun(@(x, y) x(y), bpodEventTimes, goodTimes);
+% end
+
+if ~isempty(excludeEventsByState)
+    goodStates = cellfun(@(x) regexp(fields(x.States), excludeEventsByState), rawEvents2Check, 'uni', 0);
+    goodStates = cellfun(@(x) cellfun(@(y) ~isempty(y), x), goodStates, 'uni', 0);
+    trialCells = cellfun(@(x) struct2cell(x.States), rawEvents2Check, 'uni', 0);
+    stateTimes = cellfun(@(x, y) x(y), trialCells, goodStates, 'uni', 0);
+    stateTimesToExclude = cellfun(@(x) cat(1, x{:}), stateTimes, 'uni', 0);
+    timesToRemove = cellfun(@(x, y) ismember(x, y), bpodEventTimes, stateTimesToExclude, 'uni', 0);
+    bpodEventTimes = cellfun(@(x, y) x(~y), bpodEventTimes, timesToRemove, 'uni', 0);
 end
+
 
 % Find bpod intra-trial times for Trial Start timestamp
 bpodStartTimes = cellfun(@(x) x.States.(obj.info.startState)(1), rawEvents2Check, 'uni', 0);
@@ -117,8 +130,8 @@ bpodStartTimes = cellfun(@(x) x.States.(obj.info.startState)(1), rawEvents2Check
 eventOffset = cellfun(@(x, y) (x - y) * obj.info.baud, bpodEventTimes, bpodStartTimes, 'uni', 0);
 % subtract the factor that bpod outpaces the blackrock system by, .0136
 eventOffsetCorrected = cellfun(@(x) x - x.*.0136, eventOffset, 'uni', 0);
-eventTimes = cellfun(@(x, y) x + y, trialStartTimes, eventOffsetCorrected, 'uni', 0);
-% eventTimes = cellfun(@(x, y) x + y, trialStartTimes, eventOffset, 'uni', 0);
+% eventTimes = cellfun(@(x, y) x + y, trialStartTimes, eventOffsetCorrected, 'uni', 0);
+eventTimes = cellfun(@(x, y) x + y, trialStartTimes, eventOffset, 'uni', 0);
 
 
 if trialized 
