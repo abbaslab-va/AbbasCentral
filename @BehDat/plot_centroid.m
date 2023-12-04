dc5d7162889376b89f2d06a3973ada43106dec80
Lowell Bartlett <bartlett.lowell@gmail.com>
2023-12-04 14:19:32 -0800

    Add timecourse info to centroid plotting

--- @BehDat/plot_centroid.m
+++ @BehDat/plot_centroid.m
@@ -17,20 +17,28 @@ else
     eventTimes = obj.find_event('preset', presets)';
 end
 
-eventCells = num2cell([-60 0] + eventTimes, 2);
+if ~isfield(obj.info, 'frameRate')
+    frameRate = 30;
+else
+    frameRate = obj.info.frameRate;
+end
+
+edges = presets.edges * frameRate;
+colors = parula(edges(2) - edges(1) + 1);
+eventCells = num2cell(edges + eventTimes, 2);
 
 coordCell = cellfun(@(x) obj.coordinates(x(1):x(2), :), eventCells, 'uni', 0);
 
 if ~isempty(presets.panel)
     h = figure('Visible', 'off');
     hold on
-    cellfun(@(x) scatter(x(:, 1), x(:, 2), 'k', 'filled'), coordCell)
+    cellfun(@(x) scatter(x(:, 1), x(:, 2), [], colors, 'filled'), coordCell)
     set(gca, 'color', 'w')
     copyobj(h.Children, presets.panel)
     close(h)
 else
     h = figure;
     hold on
-    cellfun(@(x) scatter(x(:, 1), x(:, 2), 'k', 'filled'), coordCell)
+    cellfun(@(x) scatter(x(:, 1), x(:, 2), [], colors, 'filled'), coordCell)
     set(gca, 'color', 'w')
 end
