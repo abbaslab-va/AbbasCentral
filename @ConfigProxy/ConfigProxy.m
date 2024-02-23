76b6cbfcd48271f5edba0f63f5fb7e5ab844bfb4
Lowell Bartlett <bartlett.lowell@gmail.com>
2024-02-23 15:49:17 -0800

    Instantiate ConfigProxy class

---
+++ @ConfigProxy/ConfigProxy.m
@@ -0,0 +1,21 @@
+classdef ConfigProxy < handle
+
+% This class manages the parsing of config.ini files, as well as stores the
+% configurations from the files. This allows for BehDat as well as
+% BpodParser to access the same methodology when requesting subsets of
+% data.
+
+    properties
+        outcomes
+        trialTypes
+        stimTypes
+        startState
+        regions
+    end
+
+    methods
+        goodTrials = trial_intersection(obj, presets, trializedEvents)
+
+    end
+
+end
