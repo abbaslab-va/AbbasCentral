4ae64efd630af4b0acaaf6635895b24751e0d0e3
Lowell Bartlett <bartlett.lowell@gmail.com>
2024-05-13 14:34:10 -0700

    Update HCTSA position initialization
    Data is now automatically augmented to face the same way relative to the delay port in the 3-port tri-box task

--- @BehDat/hctsa_position_initialize.m
+++ @BehDat/hctsa_position_initialize.m
@@ -3,17 +3,29 @@ function [timeSeriesData, labels, keywords] = hctsa_position_initialize(obj, pre
 coordinates = obj.plot_centroid('event', presets.event, 'edges', presets.edges, ...
     'offset', presets.offset, 'plot', false);
 vidCenter = obj.info.vidRes/2;
-% delay2Trials = obj.bpod.trial_intersection_BpodParser('trialType', 'Delay2');
-% delay3Trials = obj.bpod.trial_intersection_BpodParser('trialType', 'Delay3');
-% rot2 = rotate_coordinate_data(coordinates(delay2Trials), vidCenter, 2*pi/3);
-% rot3 = rotate_coordinate_data(coordinates(delay3Trials), vidCenter, 4*pi/3);
-% coordinates(delay2Trials) = rot2;
-% coordinates(delay3Trials) = rot3;
-badCoords = cellfun(@(x) x == 0, coordinates, 'uni', 0);
+leftTrials = obj.bpod.trial_intersection_BpodParser('trialType', 'Left');
+rightTrials = obj.bpod.trial_intersection_BpodParser('trialType', 'Right');
+correctTrials = obj.bpod.trial_intersection_BpodParser('outcome', 'Correct');
+delay1Trials = obj.bpod.trial_intersection_BpodParser('trialType', 'Delay1');
+delay2Trials = obj.bpod.trial_intersection_BpodParser('trialType', 'Delay2');
+delay3Trials = obj.bpod.trial_intersection_BpodParser('trialType', 'Delay3');
+rot1 = rotate_coordinate_data(coordinates(delay1Trials), vidCenter, -2*pi/3);
+rot3 = rotate_coordinate_data(coordinates(delay3Trials), vidCenter, 2*pi/3);
+coordinates(delay1Trials) = rot1;
+coordinates(delay3Trials) = rot3;
+negCoords = cellfun(@(x) x <= 0, coordinates, 'uni', 0);
+badX = cellfun(@(x) x(:, 1) > obj.info.vidRes(1), coordinates, 'uni', 0);
+badY = cellfun(@(y) y(:, 2) > obj.info.vidRes(2), coordinates, 'uni', 0);
 for t = 1:numel(coordinates)
-    coordinates{t}(badCoords{t}) = 1;
+    coordinates{t}(negCoords{t}) = 1;
+    coordinates{t}(badX{t}, 1) = obj.info.vidRes(1);
+    coordinates{t}(badY{t}, 2) = obj.info.vidRes(2);
 end
-timeSeriesData = cellfun(@(x) sub2ind(obj.info.vidRes, round(x(:, 1)), round(x(:, 2))), coordinates, 'uni', 0);
+
+% timeSeriesData = cellfun(@(x) sub2ind(obj.info.vidRes, round(x(:, 1)), round(x(:, 2))), coordinates, 'uni', 0);
+% timeSeriesData = cellfun(@(x) sub2ind(obj.info.vidRes, flipud(round(x(:, 1))), round(x(:, 2))), coordinates, 'uni', 0);
+timeSeriesData = cellfun(@(x) sub2ind(fliplr(obj.info.vidRes), round(x(:, 2)), round(x(:, 1))), coordinates, 'uni', 0);
+timeSeriesData = cellfun(@(x) x - x(1), timeSeriesData, 'uni', 0);
 trialNo = num2cell(1:numel(timeSeriesData));
 trialString = cellfun(@(x) num2str(x), trialNo, 'uni', 0);
 [~, sessName] = fileparts(obj.info.path);
@@ -23,3 +35,34 @@ keywords = obj.hctsa_keywords('preset', presets);
 if numel(keywords) ~= numel(labels) || numel(keywords) ~= numel(timeSeriesData)
     disp('poop')
 end
+
+% figure
+% set(gca, 'Color', 'w')
+% hold on
+% cellfun(@(x) scatter(x(:, 1), x(:, 2), 'r'), coordinates(delay1Trials & leftTrials & correctTrials))
+% cellfun(@(x) scatter(x(:, 1), x(:, 2), 'b'), coordinates(delay2Trials & leftTrials & correctTrials))
+% cellfun(@(x) scatter(x(:, 1), x(:, 2), 'g'), coordinates(delay3Trials & leftTrials & correctTrials))
+% cellfun(@(x) scatter(x(:, 1), x(:, 2), 'MarkerFaceColor', [.5 0 0], 'MarkerEdgeColor', [.5 0 0]), coordinates(delay1Trials & rightTrials & correctTrials))
+% cellfun(@(x) scatter(x(:, 1), x(:, 2), 'MarkerFaceColor', [0 0 .5], 'MarkerEdgeColor', [0 0 .5]), coordinates(delay2Trials & rightTrials & correctTrials))
+% cellfun(@(x) scatter(x(:, 1), x(:, 2), 'MarkerFaceColor', [0 .5 0], 'MarkerEdgeColor', [0 .5 0]), coordinates(delay3Trials & rightTrials & correctTrials))
+% figure
+% set(gca, 'Color', 'w')
+% hold on
+% cellfun(@(x) plot(x, 'r'), timeSeriesData(delay1Trials & leftTrials & correctTrials))
+% cellfun(@(x) plot(x, 'b'), timeSeriesData(delay2Trials & leftTrials & correctTrials))
+% cellfun(@(x) plot(x, 'g'), timeSeriesData(delay3Trials & leftTrials & correctTrials))
+% cellfun(@(x) plot(x, 'Color', [.5 0 0]), timeSeriesData(delay1Trials & rightTrials & correctTrials))
+% cellfun(@(x) plot(x, 'Color', [0 0 .5]), timeSeriesData(delay2Trials & rightTrials & correctTrials))
+% cellfun(@(x) plot(x, 'Color', [0 .5 0]), timeSeriesData(delay3Trials & rightTrials & correctTrials))
+% 
+% timeSeriesData = timeSeriesData(correctTrials);
+% labels = labels(correctTrials);
+% keywords = keywords(correctTrials);
+
+% timeSeriesData = timeSeriesData(leftTrials);
+% labels = labels(leftTrials);
+% keywords = keywords(leftTrials);
+% % 
+timeSeriesData = timeSeriesData(rightTrials);
+labels = labels(rightTrials);
+keywords = keywords(rightTrials);
--- @ExpManager/hctsa_position.m
+++ @ExpManager/hctsa_position.m
@@ -5,29 +5,39 @@ function hctsa_position(obj, varargin)
 
 cd('E:/Ephys/Test')
 presets = PresetManager(varargin{:});
+fileDir = dir;
+fileNames = extractfield(fileDir, 'name');
+
+
 whichSessions = obj.subset(presets.animals);
-subNames = join(presets.animals, '+');
-[timeSeriesData, labels, keywords] = arrayfun(@(x) x.hctsa_position_initialize(presets), obj.sessions(whichSessions), 'uni', 0);
-timeSeriesData = cat(1, timeSeriesData{:});
-labels = cat(2, labels{:})';
-keywords = cat(2, keywords{:})';
-
-
+if ~isempty(presets.animals)
+    subNames = join(presets.animals, '+');
+end
 saveString = strcat('hctsa_position_all_', presets.event, '_', subNames);
 saveFile = strcat(saveString, '.mat');
 normalizedFile = strcat(saveString, '_N.mat');
-save(saveFile, 'timeSeriesData', 'labels', 'keywords');
-TS_Init(saveFile, {'E:\AbbasCentral\packages\hctsa\FeatureSets\INP_mops_catch24.txt', ...
-    'E:\AbbasCentral\packages\hctsa\FeatureSets\INP_ops_catch24.txt'}, true, saveFile, false);
-sample_runscript_matlab(true, 5, saveFile);
-% TS_LabelGroups('raw',labels);
-TS_Normalize('mixedSigmoid',[0.5, 1.0], saveFile);    
-% Cluster
-distanceMetricRow = 'euclidean'; % time-series feature distance
-linkageMethodRow = 'average'; % linkage method
-distanceMetricCol = 'corr_fast'; % a (poor) approximation of correlations with NaNs
-linkageMethodCol = 'average'; % linkage method
-TS_Cluster(distanceMetricRow, linkageMethodRow, distanceMetricCol, linkageMethodCol, true, normalizedFile);
+
+% if ~ismember(normalizedFile, fileNames)
+
+    [timeSeriesData, labels, keywords] = arrayfun(@(x) x.hctsa_position_initialize(presets), obj.sessions(whichSessions), 'uni', 0);
+    timeSeriesData = cat(1, timeSeriesData{:});
+    labels = cat(2, labels{:})';
+    keywords = cat(2, keywords{:})';
+    
+    
+    save(saveFile, 'timeSeriesData', 'labels', 'keywords');
+    TS_Init(saveFile, {'E:\AbbasCentral\packages\hctsa\FeatureSets\INP_mops_catch24.txt', ...
+        'E:\AbbasCentral\packages\hctsa\FeatureSets\INP_ops_catch24.txt'}, true, saveFile, false);
+    sample_runscript_matlab(true, 5, saveFile);
+    % TS_LabelGroups('raw',labels);
+    TS_Normalize('mixedSigmoid',[0.5, 1.0], saveFile);    
+    % Cluster
+    distanceMetricRow = 'euclidean'; % time-series feature distance
+    linkageMethodRow = 'average'; % linkage method
+    distanceMetricCol = 'corr_fast'; % a (poor) approximation of correlations with NaNs
+    linkageMethodCol = 'average'; % linkage method
+    TS_Cluster(distanceMetricRow, linkageMethodRow, distanceMetricCol, linkageMethodCol, true, normalizedFile);
+% end
 
 if isempty(presets.region)
     regionLabel = {};
@@ -57,7 +67,7 @@ groupLabels = [outcomeLabel, ttLabel, regionLabel];
 try
     % Visualize
     TS_LabelGroups(normalizedFile, groupLabels);
-    TS_PlotDataMatrix('whatData', normalizedFile);
+    % TS_PlotDataMatrix('whatData', normalizedFile, 'colorGroups', true);
     if ~isempty(presets.panel)
         figH = TS_PlotLowDim(normalizedFile);
         set(gca, 'color', 'w')
