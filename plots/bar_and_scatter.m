b656b957db83a1f5366bbbf6b0dbee8035735367
Lowell Bartlett <bartlett.lowell@gmail.com>
2024-07-11 17:14:31 -0700

    Update make_BpodParser_array fcns
    Makes it easier to to batch analysis on training data that has been collected using the script CompileLocalData

--- plots/bar_and_scatter.m
+++ plots/bar_and_scatter.m
@@ -1,4 +1,4 @@
-function bar_and_scatter(data)
+function figH = bar_and_scatter(data, varargin)
 
 if ~iscell(data)
     dataSize = size(data);
@@ -7,14 +7,25 @@ if ~iscell(data)
     % dataMeans = mean(data);
     % dataSEM = std(data)/sqrt(numel(data));
 end
+p = inputParser;
+addParameter(p, 'color', 'k')
+addParameter(p, 'alpha', .6)
+parse(p, varargin{:});
+a = p.Results;
 dataMeans = cellfun(@(x) mean(x, 'omitnan'), data);
 dataSEM = cellfun(@(x) std(x, 0, 'omitnan')/sqrt(numel(x(~isnan(x)))), data);
-figure
+figH = figure;
 hold on
-bar(dataMeans, 'FaceColor', 'k', 'FaceAlpha', .5, 'EdgeColor', 'k', 'EdgeAlpha', 1)
+if ~iscell(a.color)
+    bar(dataMeans, 'FaceColor', a.color, 'FaceAlpha', a.alpha, 'EdgeColor', a.color, 'EdgeAlpha', a.alpha)
+else
+    for c = 1:numel(a.color)
+        bar(c, dataMeans(c), 'FaceColor', a.color{c}, 'FaceAlpha', a.alpha, 'EdgeColor', a.color{c}, 'EdgeAlpha', a.alpha)
+    end
+end
 for s = 1:numel(dataMeans)
     jitterVec = (zeros(1, numel(data{s})) + rand(1, numel(data{s})) - .5).*.5;
-    scatter(zeros(1, numel(data{s})) + s + jitterVec, data{s}, 20, 'filled', 'r')
+    scatter(zeros(1, numel(data{s})) + s + jitterVec, data{s}, 20, 'filled', 'k')
 end
-errorbar(dataMeans, dataSEM, 'vertical', 'Color','k', 'LineWidth', 1.5, 'LineStyle', 'none');
+errorbar(dataMeans, dataSEM, 'vertical', 'Color','k', 'LineWidth', 3, 'LineStyle', 'none');
 set(gca, 'Color', 'w')
